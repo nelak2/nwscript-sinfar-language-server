1dfda0045a221e121221fba150a7965faa097dad
nelak2 <hello468@hotmail.com>
2022-12-11 01:25:37 -0700

    Placeholder colorpicker

--- client/src/components/nwnColorPicker.ts
+++ client/src/components/nwnColorPicker.ts
@@ -1,5 +1,37 @@
-export class nwnColorPicker extends HTMLDivElement {
+import { buildLabel, buildDiv } from "./utils";
+
+export class nwnColorPicker extends HTMLElement {
   constructor() {
     super();
+
+    const id = this.getAttribute("id");
+    const text = this.getAttribute("text");
+
+    if (!id || !text) {
+      return;
+    }
+
+    const label = buildLabel(text, id);
+    label.className = "vscode-input-label";
+
+    const divColLabel = buildDiv("col-label");
+    divColLabel.appendChild(label);
+
+    const textField = document.createElement("vscode-text-field");
+    textField.id = id;
+    textField.setAttribute("style", "width: 300px");
+    const disabled = this.getAttribute("disabled");
+    if (disabled === "true") {
+      textField.setAttribute("disabled", "true");
+    }
+
+    const divColInput = buildDiv("col-input");
+    divColInput.appendChild(textField);
+
+    const rowDiv = buildDiv("row");
+    rowDiv.appendChild(divColLabel);
+    rowDiv.appendChild(divColInput);
+
+    this.appendChild(rowDiv);
   }
 }
