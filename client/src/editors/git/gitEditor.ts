8e945a9f0ba55766042106135541638379138e44
nelak2 <hello468@hotmail.com>
2022-12-12 16:08:58 -0700

    Started work on binding fields

--- client/src/editors/git/gitEditor.ts
+++ client/src/editors/git/gitEditor.ts
@@ -43,6 +43,7 @@ function InboundMessageHandler(event: any) {
       case "update":
         try {
           content = JSON.parse(message.text);
+          BindFields(content);
           test3.innerText = "Data: " + JSON.stringify(content);
         } catch {
           test3.innerText = "Data: failed to parse";
@@ -54,3 +55,90 @@ function InboundMessageHandler(event: any) {
     }
   }
 }
+
+type Area = {
+  resName: string;
+  erfId: number;
+  resData: [];
+  langId: string;
+  resResRef: string;
+  editableFields: any;
+  extraData: any;
+};
+
+type ResData = {
+  AreaProperties: [[AreaProperties]];
+  ["Creature List"]: any;
+  ["Door List"]: any;
+  ["Encounter List"]: any;
+  List: any;
+  ["Placeable List"]: any;
+  SoundList: any;
+  StoreList: any;
+  TriggerList: any;
+  VarTable: any;
+  WaypointList: any;
+};
+
+type AreaProperties = {
+  AmbientSndDay: [];
+  AmbientSndDayVol: [];
+  AmbientSndNight: [];
+  AmbientSndNitVol: [];
+  EnvAudio: [];
+  MusicBattle: [];
+  MusicDay: [];
+  MusicDelay: [];
+  MusicNight: [];
+};
+
+function BindFields(content: Area) {
+  const resdata = content.resData[1] as unknown as ResData;
+  const areaProp = resdata.AreaProperties.at(1)?.at(1) as AreaProperties;
+
+  console.clear();
+  console.log(areaProp.AmbientSndDay[1]);
+}
+
+// res_Name
+// res_Tag
+// res_TilesetResRef
+// res_Size
+// res_DayNightCycle
+// res_Flags_SunShadows
+// res_Flags_MoonShadows
+// res_SunAmbientColor
+// res_SunDiffuseColor
+// res_SunFogAmount
+// res_SunFogColor
+// res_MoonAmbientColor
+// res_MoonDiffuseColor
+// res_MoonFogAmount
+// res_MoonFogColor
+// res_FogClipDistance
+// res_ShadowOp
+// res_ShadowOpacity
+// res_WeatherWindPower
+// res_WeatherSnowPercentage
+// res_WeatherRainPercentage
+// res_WeatherLightningPercentage
+// res_Skybox
+// res_AmbientSndDay
+// res_AmbientSndDayVol
+// res_MusicDay
+// res_AmbientSndNight
+// res_AmbientSndNightVol
+// res_MusicNight
+// res_MusicBattle
+// res_MusicDelay
+// res_EnvAudio
+// res_Flags_NoRest
+// res_Flags_Interior
+// res_Flags_Underground
+// res_Flags_Natural
+// res_CheckModifierListen
+// res_CheckModifierSpot
+// res_PvP
+// res_LoadScreen
+// SCRIPTS
+// VARIABLES
