36364611c834c5c442a754038657220069892f78
nelak2 <hello468@hotmail.com>
2023-01-04 20:56:33 -0700

    Added event dispatching and handling field updates

--- client/src/components/nwnVariables.ts
+++ client/src/components/nwnVariables.ts
@@ -1,10 +1,10 @@
-import { buildLabel, buildDiv, buildButton } from "./utils";
+import { buildLabel, buildDiv, buildButton, buildDropdown, buildTextField } from "./utils";
 import { VarType } from "./lists/index";
 
 type Variable = {
   name: string;
-  varType: string;
-  value: string;
+  varType: number;
+  value: string | number;
 };
 
 export class nwnVariables extends HTMLElement {
@@ -16,14 +16,15 @@ export class nwnVariables extends HTMLElement {
 
   _variables: Variable[];
   _variableListDiv: HTMLDivElement;
+
   constructor() {
     super();
 
     let variables: Variable[] = [];
     try {
       variables = JSON.parse(this.getAttribute("variables") ?? "[]");
-    } catch {
-      console.log("Error parsing variables");
+    } catch (error: any) {
+      window.dispatchEvent(new CustomEvent("alert", { detail: error.message }));
     }
 
     this._variables = variables;
@@ -52,10 +53,10 @@ export class nwnVariables extends HTMLElement {
 
     // create new variable row
     const variableAddRow = buildDiv("variableRow");
-    const textFieldName = this.buildTextField("");
-    const dropdownType = this.buildDropdown(VarType[0].value);
-    const textFieldValue = this.buildTextField("");
-    const buttonAdd = buildButton("add");
+    const textFieldName = buildTextField("", "nwnvartable_add_name");
+    const dropdownType = buildDropdown(VarType[0].value, "nwnvartable_add_type");
+    const textFieldValue = buildTextField("", "nwnvartable_add_value");
+    const buttonAdd = buildButton("add", "vartable_add_btn");
     variableAddRow.appendChild(textFieldName);
     variableAddRow.appendChild(dropdownType);
     variableAddRow.appendChild(textFieldValue);
@@ -63,64 +64,122 @@ export class nwnVariables extends HTMLElement {
     this.appendChild(variableAddRow);
 
     // add event listeners
-    buttonAdd.addEventListener("click", () => {
-      this.addVariable(textFieldName, dropdownType, textFieldValue);
+    buttonAdd.addEventListener("click", (e) => {
+      this.addVariable(e, textFieldName, dropdownType, textFieldValue);
     });
 
     this.refreshVariables();
   }
 
-  onChildChanged(e: Event) {
-    this.dispatchEvent(new CustomEvent("varTableChanged", { bubbles: true, composed: true, cancelable: true }));
+  // update fields when their values change
+  onFieldChanged(fieldId: number, changeType: string, e: Event) {
+    const newValue = (e.target as HTMLInputElement).value;
+
+    switch (changeType) {
+      case "name": {
+        // validate name
+        try {
+          this.validateVariable(newValue, this._variables[fieldId].varType, this._variables[fieldId].value, true);
+        } catch (error: any) {
+          window.dispatchEvent(new CustomEvent("alert", { detail: error.message }));
+
+          // revert to old value
+          (e.target as HTMLInputElement).value = this._variables[fieldId].name;
+        }
+
+        this._variables[fieldId].name = newValue;
+        break;
+      }
+      case "type": {
+        try {
+          this.validateVariable(this._variables[fieldId].name, parseInt(newValue), this._variables[fieldId].value);
+        } catch (error: any) {
+          window.dispatchEvent(new CustomEvent("alert", { detail: error.message }));
+
+          // revert to old value
+          (e.target as HTMLSelectElement).selectedIndex = this._variables[fieldId].varType - 1;
+        }
+
+        this._variables[fieldId].varType = (e.target as HTMLSelectElement).selectedIndex + 1;
+
+        break;
+      }
+      case "value":
+        try {
+          this.validateVariable(this._variables[fieldId].name, this._variables[fieldId].varType, newValue);
+        } catch (error: any) {
+          window.dispatchEvent(new CustomEvent("alert", { detail: error.message }));
+
+          // revert to old value
+          (e.target as HTMLInputElement).value = this._variables[fieldId].value.toString();
+        }
+
+        this._variables[fieldId].value = newValue;
+        break;
+    }
+
+    // Make sure value field is formatted correctly
+    const valueField = document.getElementById(`var_value_${fieldId}`) as HTMLInputElement;
+    const currentValue = valueField.getAttribute("current-value") ?? "0";
+    valueField.setAttribute("current-value", this.formatValue(currentValue, this._variables[fieldId].varType));
   }
 
-  addVariable(textFieldName: HTMLElement, dropdownType: HTMLElement, textFieldValue: HTMLElement) {
-    // check if variable already exists
-    const name = textFieldName.getAttribute("current-value") || "";
+  validateVariable(name: string, varType: number, value: string | number, checkNameExists = false) {
+    // check variable name is not empty
     if (name === "") {
-      console.log("Variable name cannot be empty");
-      return;
+      throw new Error("Variable name cannot be empty");
     }
 
-    if (this._variables.find((variable) => variable.name === name)) {
-      console.log("Variable already exists");
-      return;
+    // check variable name is valid
+    if (!/^[a-zA-Z_][a-zA-Z0-9_]*$/.test(name)) {
+      throw new Error("Variable name is invalid");
     }
 
-    // validate type
-    const varType = dropdownType.getAttribute("current-value") || "";
-    if (varType === "") {
-      console.log("Variable type cannot be empty");
-      return;
+    if (checkNameExists && this._variables.find((variable) => variable.name === name)) {
+      throw new Error("Variable already exists");
+    }
+
+    // validate type;
+    if (varType === 0) {
+      throw new Error("Variable type cannot be empty");
     }
 
     // validate value
-    let value = textFieldValue.getAttribute("current-value") || "";
     if (value === "") {
-      console.log("Variable value cannot be empty");
-      return;
+      throw new Error("Variable value cannot be empty");
     }
 
-    console.log("name: " + name);
-    console.log("varType: " + varType);
-    console.log("value: " + value);
-    if (varType === "1" && isNaN(parseInt(value))) {
-      console.log("Variable value must be an integer");
-      return;
-    } else if (varType === "2" && isNaN(parseFloat(value))) {
-      console.log("Variable value must be a float");
-      return;
+    if (varType === 1 && isNaN(parseInt(value as string))) {
+      throw new Error("Variable value must be an integer");
+    } else if (varType === 2 && isNaN(parseFloat(value as string))) {
+      throw new Error("Variable value must be a float");
     }
+  }
 
+  formatValue(value: string, varType: number): string {
     // Cast to int/float and back to string to remove decimal places for ints
     // and add trailing 0s for floats
-    if (varType === "1") {
-      const intValue = parseInt(value);
-      value = intValue.toString();
-    } else if (varType === "2") {
-      const floatValue = parseFloat(value);
-      value = floatValue.toString();
+    if (varType === 1) {
+      return parseInt(value).toString();
+    } else if (varType === 2) {
+      return parseFloat(value).toString();
     }
+    return value;
+  }
+
+  addVariable(e: Event, textFieldName: HTMLElement, dropdownType: HTMLElement, textFieldValue: HTMLElement) {
+    const name = textFieldName.getAttribute("current-value") || "";
+    const varType = parseInt(dropdownType.getAttribute("current-value") || "0");
+    let value = textFieldValue.getAttribute("current-value") || "";
+
+    try {
+      this.validateVariable(name, varType, value, true);
+    } catch (error: any) {
+      window.dispatchEvent(new CustomEvent("alert", { detail: error.message }));
+      return;
+    }
+
+    value = this.formatValue(value, varType);
 
     this._variables.push({
       name,
@@ -128,19 +187,26 @@ export class nwnVariables extends HTMLElement {
       value,
     });
     this.refreshVariables();
+
+    // Dispatch event to notify parent that variable table has changed
+    e.target?.dispatchEvent(new Event("change", { bubbles: true, composed: true, cancelable: true }));
   }
 
-  deleteVariable(variableRow: HTMLElement) {
+  deleteVariable(e: Event, variableRow: HTMLElement) {
+    // Dispatch event to notify parent that variable table has changed
+    e.target?.dispatchEvent(new Event("change", { bubbles: true, composed: true, cancelable: true }));
+
     const name = variableRow.children[0].getAttribute("current-value") || "";
     this._variables = this._variables.filter((variable) => variable.name !== name);
-    this.removeRow(variableRow);
+    this.refreshVariables();
   }
 
   refreshVariables() {
     this._variableListDiv.replaceChildren();
     // create variable rows
-    for (const variable of this._variables) {
-      this.addRow(variable);
+
+    for (let i = 0; i < this._variables.length; i++) {
+      this.addRow(i.toString(), this._variables[i]);
     }
   }
 
@@ -159,27 +225,27 @@ export class nwnVariables extends HTMLElement {
     return [15, variableArray];
   }
 
-  getNWNGFFType(varType: string): number {
+  getNWNGFFType(varType: number): number {
     // For some reason doing the comparison with === always returns false
     // Not clear why, but this works for now
     // eslint-disable-next-line eqeqeq
-    if (varType == "1") return 5; // int
+    if (varType == 1) return 5; // int
     // eslint-disable-next-line eqeqeq
-    if (varType == "2") return 8; // float
+    if (varType == 2) return 8; // float
     // eslint-disable-next-line eqeqeq
-    if (varType == "3") return 10; // string
+    if (varType == 3) return 10; // string
 
     return 10; // default to string
   }
 
-  addRow(variable: Variable) {
+  addRow(fieldId: string, variable: Variable) {
     if (!this._variableListDiv) return;
 
     const variableRow = buildDiv("variableRow");
-    const textFieldName = this.buildTextField(variable.name);
-    const dropdownType = this.buildDropdown(variable.varType);
-    const textFieldValue = this.buildTextField(variable.value);
-    const buttonDelete = buildButton("close");
+    const textFieldName = buildTextField(variable.name, "var_name_" + fieldId);
+    const dropdownType = buildDropdown(variable.varType.toString(), "var_type_" + fieldId);
+    const textFieldValue = buildTextField(variable.value.toString(), "var_value_" + fieldId);
+    const buttonDelete = buildButton("close", "var_del_" + fieldId);
     variableRow.appendChild(textFieldName);
     variableRow.appendChild(dropdownType);
     variableRow.appendChild(textFieldValue);
@@ -187,35 +253,22 @@ export class nwnVariables extends HTMLElement {
     this._variableListDiv.appendChild(variableRow);
 
     // add event listeners
-    buttonDelete.addEventListener("click", () => {
-      this.deleteVariable(variableRow);
+    buttonDelete.addEventListener("click", (e) => {
+      this.deleteVariable(e, variableRow);
+    });
+    textFieldName.addEventListener("change", (e) => {
+      this.onFieldChanged(Number.parseInt(fieldId), "name", e);
+    });
+    dropdownType.addEventListener("change", (e) => {
+      this.onFieldChanged(Number.parseInt(fieldId), "type", e);
+    });
+    textFieldValue.addEventListener("change", (e) => {
+      this.onFieldChanged(Number.parseInt(fieldId), "value", e);
     });
   }
 
   removeRow(variableRow: HTMLElement) {
     this._variableListDiv.removeChild(variableRow);
-  }
-
-  buildTextField(value: string): HTMLElement {
-    const textField = document.createElement("vscode-text-field");
-    textField.className = "variableField";
-    textField.setAttribute("value", value);
-    return textField;
-  }
-
-  buildDropdown(value: string): HTMLElement {
-    const dropdown = document.createElement("vscode-dropdown");
-    dropdown.className = "varTypeDropdown";
-
-    for (const type of VarType) {
-      const option = document.createElement("vscode-option");
-      option.setAttribute("value", type.value);
-      option.textContent = type.label;
-      dropdown.appendChild(option);
-    }
-
-    dropdown.setAttribute("current-value", value);
-    return dropdown;
   }
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
@@ -235,34 +288,3 @@ export class nwnVariables extends HTMLElement {
     }
   }
 }
-
-// <div class="variableRow">
-//   <label for="variablesList" style="width: 162.5px; padding-inline: 5px">Name</label>
-//   <label class="vscode-input-label-varheader" style="width: 70px; padding-inline: 5px" for="variablesList">Type</label>
-//   <label class="vscode-input-label-varheader" style="padding-inline: 5px" for="variablesList">Value</label>
-// </div>
-// <div class="variableRow">
-//   <vscode-text-field class="variableField"></vscode-text-field>
-//   <vscode-dropdown class="varTypeDropdown">
-//     <vscode-option value="1">Int</vscode-option>
-//     <vscode-option value="2">Float</vscode-option>
-//     <vscode-option value="3">String</vscode-option>
-//   </vscode-dropdown>
-//   <vscode-text-field class="variableField"></vscode-text-field>
-//   <vscode-button appearance="icon" aria-label="Delete">
-//     <span class="codicon codicon-close"></span>
-//   </vscode-button>
-// </div>
-// <vscode-divider style="width: 455px"></vscode-divider>
-// <div class="variableRow">
-//   <vscode-text-field class="variableField"></vscode-text-field>
-//   <vscode-dropdown class="varTypeDropdown">
-//     <vscode-option value="1">Int</vscode-option>
-//     <vscode-option value="2">Float</vscode-option>
-//     <vscode-option value="3">String</vscode-option>
-//   </vscode-dropdown>
-//   <vscode-text-field class="variableField"></vscode-text-field>
-//   <vscode-button appearance="icon" aria-label="Add">
-//   <span class="codicon codicon-add"></span>
-//   </vscode-button>
-// </div>
