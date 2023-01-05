6f7ed02d4e6457a7e9b8111d94d004dcdead0d2d
nelak2 <hello468@hotmail.com>
2023-01-04 20:56:58 -0700

    added buildTextField and buildDropdown

--- client/src/components/utils.ts
+++ client/src/components/utils.ts
@@ -1,3 +1,5 @@
+import { VarType } from "./lists/index";
+
 export function buildLabel(text: string, htmlFor: string): HTMLLabelElement {
   const label = document.createElement("label");
   label.className = "vscode-input-label";
@@ -13,14 +15,40 @@ export function buildDiv(className: string): HTMLDivElement {
 }
 
 // Build icon buttons using vscode codicons
-export function buildButton(type: string): HTMLElement {
+export function buildButton(type: string, id: string = ""): HTMLElement {
   const button = document.createElement("vscode-button");
+  button.id = id;
   button.setAttribute("appearance", "icon");
   button.setAttribute("aria-label", type);
 
   const span = document.createElement("span");
+  span.id = id;
   span.className = "codicon codicon-" + type;
   button.appendChild(span);
 
   return button;
+}
+
+export function buildTextField(value: string, fieldId: string = ""): HTMLElement {
+  const textField = document.createElement("vscode-text-field");
+  textField.id = fieldId;
+  textField.className = "variableField";
+  textField.setAttribute("value", value);
+  return textField;
+}
+
+export function buildDropdown(value: string, fieldId: string = ""): HTMLElement {
+  const dropdown = document.createElement("vscode-dropdown");
+  dropdown.id = fieldId;
+  dropdown.className = "varTypeDropdown";
+
+  for (const type of VarType) {
+    const option = document.createElement("vscode-option");
+    option.setAttribute("value", type.value);
+    option.textContent = type.label;
+    dropdown.appendChild(option);
+  }
+
+  dropdown.setAttribute("current-value", value);
+  return dropdown;
 }
