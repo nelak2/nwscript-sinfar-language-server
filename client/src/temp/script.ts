368b659a1a51ed6a8c84051fadf99f9b55727dc4
nelak2 <hello468@hotmail.com>
2023-01-14 21:04:43 -0700

    Searching implemented and tested

--- client/src/temp/script.ts
+++ client/src/temp/script.ts
@@ -1,6 +1,7 @@
 import { DropdownListItem, PLCAppearances } from "./components/lists/index";
-import * as fs from "node:fs";
 import * as _ from "lodash";
+import { Index } from "flexsearch";
+// import Worker from 'flexsearch';
 
 const ITEM_HEIGHT = 22;
 
@@ -8,41 +9,111 @@ class dropDown {
   _list: DropdownListItem[] = [];
   _optionBox: HTMLElement;
   _self: HTMLElement;
-  _search: HTMLInputElement;
+  _searchBox: HTMLInputElement;
+  _placeholder: HTMLDivElement;
 
   _start: number = 0;
   _end: number = 0;
+
+  _search: any;
+  _filtered: boolean = false;
 
   constructor() {
     this._list = PLCAppearances;
   }
 
+  public async InitSearch() {
+    this._search = new Index("score");
+
+    let i = 0;
+    for (const item of PLCAppearances) {
+      void this._search.addAsync(i, item.label);
+      i++;
+    }
+  }
+
   public load(element: HTMLElement) {
     this._self = element;
     this._optionBox = this._self.querySelector(".option-box");
-    this._search = this._self.querySelector(".search-box");
+    this._searchBox = this._self.querySelector(".search-box");
+    const codicon = this._self.querySelector(".codicon");
+
+    // Initialize search
+    void this.InitSearch();
 
     // Ensure we have a selected item set
     this._self.setAttribute("selecteditem", "0");
-    this._search.value = PLCAppearances[0].label;
+    this._searchBox.value = dropdown._list[0].label;
 
     // Set the height of the placeholder. This ensures our scroll bar is the right size
-    const placeholder = this._optionBox.querySelector(".option-placeholder") as HTMLDivElement;
-    placeholder.style.height = (this._list.length * ITEM_HEIGHT).toString() + "px";
+    this._placeholder = this._optionBox.querySelector(".option-placeholder") as HTMLDivElement;
+    this._placeholder.style.height = (this._list.length * ITEM_HEIGHT).toString() + "px";
 
     void this.LoadOptions(0, 100);
 
     // Setup event listeners
-    this._search.addEventListener("focus", this.searchBoxFocusHandler);
-    this._search.addEventListener("blur", this.searchBoxBlurHandler);
-    this._search.addEventListener("keydown", this.searchBoxKeyDownHandler);
+    this._searchBox.addEventListener("focus", this.searchBoxFocusHandler);
+    this._searchBox.addEventListener("blur", this.searchBoxBlurHandler);
+    this._searchBox.addEventListener("keydown", this.searchBoxKeyDownHandler);
+    this._searchBox.addEventListener("input", this.searchBoxInputHandler);
+    codicon.addEventListener("mousedown", this.codiconClickHandler);
 
     this._optionBox.addEventListener("mousedown", this.optionBoxMouseDownHandler);
     this._optionBox.addEventListener("scroll", _.debounce(this.optionBoxScrollHandler, 200));
   }
 
-  searchBoxKeyDownHandler(e: KeyboardEvent) {
+  searchBoxInputHandler(e: Event) {
+    const options = dropdown._optionBox;
+    const filter = dropdown._searchBox.value;
+
+    const results = dropdown._search.search(filter, {
+      limit: PLCAppearances.length,
+    });
+
+    if (results.length === 0) {
+      return;
+    }
+
+    dropdown._filtered = true;
+    dropdown._list = [];
+
+    // build filtered list
+    for (const result of results) {
+      const value = PLCAppearances[result].value;
+      const label = PLCAppearances[result].label;
+      dropdown._list.push({ value, label });
+    }
+
+    // load filtered list
+    const first = options.firstElementChild;
+
+    let sibling = first.nextElementSibling;
+    while (sibling) {
+      options.removeChild(sibling);
+      sibling = first.nextElementSibling;
+    }
+
+    let end: number = 0;
+    for (let i = 0; i < dropdown._list.length && i < 100; i++) {
+      const index = results[i];
+      // console.log({i, index});
+
+      const option = dropdown.buildOption(i);
+      // Correcct index being incorrectly set
+      option.setAttribute("index", index.toString());
+      options.appendChild(option);
+      end++;
+    }
+
+    dropdown._start = 0;
+    dropdown._end = end;
+
+    dropdown._placeholder.style.height = (dropdown._list.length * ITEM_HEIGHT).toString() + "px";
+  }
+
+  async searchBoxKeyDownHandler(e: KeyboardEvent) {
     const selectedItem = parseInt(dropdown._self.getAttribute("selecteditem"));
+    const searchBox = dropdown._searchBox;
 
     switch (e.code) {
       case "ArrowUp": {
@@ -52,13 +123,14 @@ class dropDown {
         break;
       }
       case "ArrowDown": {
-        if (selectedItem >= PLCAppearances.length - 1) return;
+        if (selectedItem >= dropdown._list.length - 1) return;
         SetSelectedOption(selectedItem + 1);
 
         break;
       }
       case "Enter": {
         closeOptionsList();
+        break;
       }
     }
   }
@@ -90,7 +162,7 @@ class dropDown {
 
     if (overlap) {
       if (endDiff > 0) {
-        for (let i = 0; i < endDiff && prevEnd + i < PLCAppearances.length; i++) {
+        for (let i = 0; i < endDiff && prevEnd + i < dropdown._list.length; i++) {
           const added = options.appendChild(this.buildOption(prevEnd + i));
         }
       } else if (endDiff < 0) {
@@ -122,9 +194,6 @@ class dropDown {
         }
       }
     } else {
-      let nodes: (string | Node)[] = [];
-      // add the placeholder child
-
       const first = options.firstElementChild;
 
       let sibling = first.nextElementSibling;
@@ -147,7 +216,7 @@ class dropDown {
     if (start < 100) start = 0;
 
     let end = center + 100;
-    if (end > PLCAppearances.length) end = PLCAppearances.length;
+    if (end > dropdown._list.length) end = dropdown._list.length;
 
     dropdown.LoadOptions(parseInt(start.toFixed(0)), parseInt(end.toFixed(0)));
   }
@@ -168,8 +237,13 @@ class dropDown {
     optionBox.style.maxHeight = "200px";
     optionBox.style.overflowY = "scroll";
 
+    dropdown._searchBox.select();
+
     // scroll to selected element
     const selected = GetCurrentOption();
+
+    //console.log("Current selected: " + selected.textContent);
+
     selected.scrollIntoView({
       behavior: "auto",
       block: "nearest",
@@ -177,10 +251,43 @@ class dropDown {
     });
   }
 
+  codiconClickHandler(e: Event) {
+    // dropdown._searchBox.focus();
+
+    const optionBox = dropdown._optionBox;
+
+    const maxHeight = optionBox.style.maxHeight;
+
+    if (maxHeight === "200px") {
+      closeOptionsList();
+    } else {
+      optionBox.style.maxHeight = "200px";
+      optionBox.style.overflowY = "scroll";
+
+      // scroll to selected element
+      const selected = GetCurrentOption();
+
+      //console.log("Current selected: " + selected.textContent);
+
+      selected.scrollIntoView({
+        behavior: "auto",
+        block: "nearest",
+        inline: "start",
+      });
+    }
+  }
+
   // Search box has lost focus
   searchBoxBlurHandler(e: Event) {
-    const start = dropdown._start;
-    const end = dropdown._end;
+    const searchBox = dropdown._searchBox;
+
+    const index = parseInt(dropdown._self.getAttribute("selecteditem"));
+
+    searchBox.value = PLCAppearances[index].label;
+
+    if (dropdown._filtered) {
+      dropdown.resetFilteredList(index);
+    }
 
     closeOptionsList();
   }
@@ -190,9 +297,28 @@ class dropDown {
 
     if (clickedElement.className !== "option-item") return;
 
-    SetSelectedOption(parseInt(clickedElement.getAttribute("index")));
+    const index = parseInt(clickedElement.getAttribute("index"));
+
+    if (dropdown._filtered) {
+      dropdown.resetFilteredList(index);
+    }
+
+    SetSelectedOption(index);
 
     closeOptionsList();
+  }
+
+  resetFilteredList(index: number) {
+    dropdown._list = [];
+    dropdown._list = PLCAppearances;
+    dropdown._filtered = false;
+
+    dropdown._placeholder.style.height = (dropdown._list.length * ITEM_HEIGHT).toString() + "px";
+
+    dropdown._start = 0;
+    dropdown._end = 0;
+
+    dropdown.RecenterOptions(index);
   }
 
   findElementWithIndex(index: number): HTMLDivElement | null {
@@ -216,10 +342,21 @@ function closeOptionsList() {
   optionBox.style.maxHeight = "0px";
 }
 
-function GetCurrentOption(): HTMLElement {
+function GetCurrentOption(): HTMLElement | null {
   const select = dropdown._self;
 
-  const index = parseInt(select.getAttribute("selecteditem"));
+  let index = parseInt(select.getAttribute("selecteditem"));
+
+  // If our selected option is outside the list entirely due to filtering
+  if (index >= dropdown._list.length) {
+    index = 0;
+  }
+
+  // If our selected option is outside the range, recenter to it
+  if (index <= dropdown._start || index > dropdown._end) {
+    dropdown.RecenterOptions(index);
+  }
+
   const selected = dropdown.findElementWithIndex(index);
 
   return selected;
@@ -227,7 +364,7 @@ function GetCurrentOption(): HTMLElement {
 
 function SetSelectedOption(index: number) {
   const select = dropdown._self;
-  const searchBox = dropdown._search;
+  const searchBox = dropdown._searchBox;
 
   dropdown.RecenterOptions(index);
 
@@ -244,7 +381,7 @@ function SetSelectedOption(index: number) {
   });
 
   select.setAttribute("selecteditem", index.toString());
-  searchBox.value = PLCAppearances[index].label;
+  searchBox.value = dropdown._list[index].label;
 }
 
 async function main() {
@@ -280,5 +417,52 @@ function VerifyList() {
 
   console.log("Verified");
 }
+
+function Find(input: string) {
+  const filter = input.toLowerCase();
+  const words = filter.split(" ");
+
+  const results: any[] = [];
+
+  for (const item of PLCAppearances) {
+    const label = item.label.toLowerCase();
+    let match: number = 0;
+
+    if (label.includes(filter)) {
+      // if it matches the full filter
+      match = match + 3;
+    }
+    // check for partial matches
+    for (const word of words) {
+      if (label.includes(word)) {
+        match++;
+      }
+    }
+    if (match > 0) {
+      results.push({ match, item });
+    }
+
+    if (results.length > 100000) {
+      break;
+    }
+  }
+  return results.sort(function (a, b) {
+    return b.match - a.match;
+  });
+}
+
+// function Search(input: string) {
+//   const searchResults = dropdown._search.search(input);
+
+//   const results: any[] = [];
+
+//   for (const item of searchResults) {
+//     const value = PLCAppearances[item].value;
+//     const label = PLCAppearances[item].label;
+//     results.push({ value, label });
+//   }
+
+//   return results;
+// }
 
 main();
