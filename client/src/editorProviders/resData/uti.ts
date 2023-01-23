627d430fd1c240c540b347ae1acee528508b9dd2
nelak2 <hello468@hotmail.com>
2023-01-22 17:49:16 -0700

    Initial uti handling

--- client/src/editorProviders/resData/uti.ts
+++ client/src/editorProviders/resData/uti.ts
@@ -2,18 +2,29 @@ import { ResData, VarTable } from ".";
 
 export class Uti extends ResData {
   private readonly _vartable: VarTable;
+  private readonly _itemProperties: ItemProperties;
 
   constructor(resdata: any) {
     super(resdata);
     this._vartable = new VarTable(this._data);
+    this._itemProperties = new ItemProperties(this._data);
   }
 
   public get editableFields() {
-    return this._data.editableFields;
+    const fields = this._data.editableFields;
+    fields.push("simple_appearance");
+    return fields;
   }
 
   public getField(field: string) {
-    return this.data.resData[1][field];
+    if (field === "AppearanceType") {
+      const baseItem = this.readField(this.data.resData[1].BaseItem);
+      return this.data.extraData.baseitems[baseItem].modelType;
+    } else if (field === "simple_appearance") {
+      return this.readField(this.data.resData[1].ModelPart1);
+    } else {
+      return this.readField(this.data.resData[1][field]);
+    }
   }
 
   public setField(field: string, value: string) {
@@ -22,5 +33,17 @@ export class Uti extends ResData {
 
   public get VarTable() {
     return this._vartable;
+  }
+
+  public get ItemProperties() {
+    return this._itemProperties;
+  }
+}
+
+class ItemProperties {
+  private readonly _data: any;
+
+  constructor(data: any) {
+    this._data = data;
   }
 }
