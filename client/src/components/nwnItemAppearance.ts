ce11f321bd6497b71c498ef26875a2f5cae75d67
nelak2 <hello468@hotmail.com>
2023-02-04 20:57:26 -0700

    Made HandleBaseItemChange public

--- client/src/components/nwnItemAppearance.ts
+++ client/src/components/nwnItemAppearance.ts
@@ -30,7 +30,7 @@ export class nwnItemAppearance extends HTMLElement {
     this.UpdateAppearanceFields();
   }
 
-  private HandleBaseItemChange() {
+  public HandleBaseItemChange() {
     this.UpdateAppearanceFields();
   }
 
