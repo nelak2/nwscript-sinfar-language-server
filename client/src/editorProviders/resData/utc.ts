d5277fe463755e40d5ec36843a223de7f18bc06b
nelak2 <hello468@hotmail.com>
2023-02-05 18:19:15 -0700

    Added reading/writing feats list

--- client/src/editorProviders/resData/utc.ts
+++ client/src/editorProviders/resData/utc.ts
@@ -1,13 +1,16 @@
 import { ResData, VarTable } from ".";
+import { Feats } from "../../components/lists";
 
 export class Utc extends ResData {
   private readonly _vartable: VarTable;
   private readonly _inventoryList: InventoryList;
+  private readonly _featList: FeatList;
 
   constructor(resdata: any) {
     super(resdata);
     this._vartable = new VarTable(this._data);
     this._inventoryList = new InventoryList(this._data);
+    this._featList = new FeatList(this._data);
   }
 
   public get editableFields() {
@@ -60,6 +63,76 @@ export class Utc extends ResData {
   public get InventoryList() {
     return this._inventoryList;
   }
+
+  public get FeatList() {
+    return this._featList;
+  }
+}
+
+class FeatList {
+  private readonly _data: any;
+  constructor(resdata: any) {
+    this._data = resdata;
+  }
+
+  public getFeatList(): { id: number; name: string }[] {
+    const feats: { id: number; name: string }[] = [];
+
+    for (const feat of this._data.resData[1].FeatList[1]) {
+      const featId: number = feat[1].Feat[1];
+      const featIdStr: string = featId.toString();
+      feats.push({ name: this.getFeatName(featIdStr) ?? featIdStr, id: featId });
+    }
+    return feats;
+  }
+
+  public getFeatName(id: string): string | undefined {
+    return Feats.find((f) => f.value === id)?.label;
+  }
+
+  public addFeat(id: number): Boolean {
+    // Don't add the feat if it already exists
+    if (this.getFeatExists(id)) return false;
+
+    const feat = [1, { Feat: [2, id] }];
+    this._data.resData[1].FeatList[1].push(feat);
+
+    return true;
+  }
+
+  private getFeatExists(id: number): Boolean {
+    return this._data.resData[1].FeatList[1].some((feat: any) => feat[1].Feat[1] === id);
+  }
+
+  public getFeatByID(id: number): number | undefined {
+    return this._data.resData[1].FeatList[1].find((feat: any) => feat[1].Feat[1] === id);
+  }
+
+  public getFeatByIndex(index: number): number | undefined {
+    return this._data.resData[1].FeatList[1][index][1].Feat[1];
+  }
+
+  public removeFeatByID(id: number): Boolean {
+    const feat = this.getFeatByID(id);
+    if (!feat) return false;
+
+    const index = this._data.resData[1].FeatList[1].indexOf(feat);
+
+    return this.deleteFeat(index);
+  }
+
+  public removeFeatByIndex(index: number): Boolean {
+    const feat = this.getFeatByIndex(index);
+    if (!feat) return false;
+
+    return this.deleteFeat(index);
+  }
+
+  private deleteFeat(index: number): Boolean {
+    this._data.resData[1].FeatList[1].splice(index, 1);
+
+    return true;
+  }
 }
 
 class InventoryList {
@@ -72,22 +145,24 @@ class InventoryList {
     const items: string[] = [];
 
     for (const item of this._data.resData[1].ItemList[1]) {
-      items.push(item[1].InventoryRes[1]);
+      let itemStr: string = item[1].InventoryRes[1];
+
+      if (item[1].Dropable[1] === 1) itemStr += " (Dropable)";
+      if (item[1].Pickpocketable[1] === 1) itemStr += " (Pickpocketable)";
+
+      items.push(itemStr);
     }
     return items;
   }
 
-  public addItem(item: string): string | undefined {
-    this._data.resData[1].ItemList[1].push([0, { InventoryRes: [11, item] }]);
-    return item;
-  }
+  public addItem(resref: string, dropable: boolean, pickpocketable: boolean): Boolean {
+    const item = [
+      0,
+      { Dropable: [0, dropable ? 1 : 0], InventoryRes: [11, resref], Pickpocketable: [0, pickpocketable ? 1 : 0] },
+    ];
+    this._data.resData[1].ItemList[1].push(item);
 
-  public updateItem(index: number, newValue: string): string | undefined {
-    const oldValue = this.getItem(index);
-    if (!oldValue) return undefined;
-
-    this._data.resData[1].ItemList[1][index] = [0, { InventoryRes: [11, newValue] }];
-    return oldValue;
+    return true;
   }
 
   public getItem(index: number): string | undefined {
